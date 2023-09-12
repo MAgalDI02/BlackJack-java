d4fca853c00f2f769bf8755be4fbbf09c92f5e25
MAgalDI02 <102761482+MAgalDI02@users.noreply.github.com>
2023-09-12 13:50:21 -0300

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,66 +1,25 @@
+public class Card {
+    private int value;
 
-public class Card 
-{
-   private final String face; // face of card ("Ace", "Deuce", ...)
-   private final String suit; // suit of card ("Hearts", "Diamonds", ...)
-   private int value;
-   // two-argument constructor initializes card's face and suit
-   public Card(String face, String suit, int value)
-   {
-      this.face = face;
-      this.suit = suit; 
-      this.value = value;
+    public Card(int value) {
+        this.value = value;
+    }
 
-   } 
+    public int getValue() {
+        if (value > 10) {
+            return 10;  // Valor das cartas de 10 a 13 (rei, rainha, valete) é 10.
+        }
+        return value;
+    }
 
-   // return String representation of Card
-   public String toString() 
-   { 
-      return face + " de " + suit;
-   } 
-
-   public int counter(String face, int value){
-      if(face == "A"){
-         value+=1;
-      }
-      if(face == "2"){
-         value+=2;
-      }
-      if(face == "3"){
-         value+=3;
-      }
-      if(face == "4"){
-         value+=4;
-      }
-      if(face == "5"){
-         value+=5;
-      }
-      if(face == "6"){
-         value+=6;
-      }
-      if(face == "7"){
-         value+=7;
-      }
-      if(face == "8"){
-         value+=8;
-      }
-      if(face == "9"){
-         value+=9;
-      }
-      if(face == "10" || face == "J" || face ==  "Q" || face == "K"){
-         value+=10;
-      }
-      return value;
-
-   }
-   
-public int handValue(){
-   return handValue(playerhand);
+    @Override
+    public String toString() {
+        if (value == 11) return "As";
+        if (value == 10) return "10";
+        if (value == 12) return "Valete";
+        if (value == 13) return "Rainha";
+        if (value == 14) return "Rei";
+        return Integer.toString(value);
+    }
 }
-public void playerturn(){
-   while (true){
-      int playerscore = 
-   }
-}
-   //fazer um if para paraatribuir os valores do 
-} // end class Card
+
