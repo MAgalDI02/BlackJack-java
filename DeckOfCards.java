1249f990dcb452ebd39f76783f86e053c51ce37c
MAgalDI02 <102761482+MAgalDI02@users.noreply.github.com>
2023-09-12 13:50:55 -0300

    Update DeckOfCards.java

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,55 +1,27 @@
-import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DeckOfCards
-{
-   private Card[] deck; // array of Card objects
-   private int currentCard; // index of next Card to be dealt (0-51)
-   private static final int NUMBER_OF_CARDS = 52; // constant # of Cards
-   // random number generator
-   private static final SecureRandom randomNumbers = new SecureRandom();
+public class DeckOfCards {
+    private List<Card> deck;
 
-   // constructor fills deck of Cards
-   public DeckOfCards()
-   {
-      String[] faces = {"A", "2", "3", "4", "5", "6", 
-         "7", "8", "9", "10", "J", "Q", "K"};
-      String[] suits = {"Copas", "Ouros", "Paus", "Espada"};
+    public DeckOfCards() {
+        deck = new ArrayList<>();
+        initializeDeck();
+    }
 
-      deck = new Card[NUMBER_OF_CARDS]; // create array of Card objects
-      currentCard = 0; // first Card dealt will be deck[0]
+    private void initializeDeck() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 2; j <= 14; j++) {
+                deck.add(new Card(j));
+            }
+        }
+    }
 
-      // populate deck with Card objects
-      for (int count = 0; count < deck.length; count++) 
-         deck[count] = 
-            new Card(faces[count % 13], suits[count / 13]);
-   } 
-
-   // shuffle deck of Cards with one-pass algorithm
-   public void shuffle()
-   {
-      // next call to method dealCard should start at deck[0] again
-      currentCard = 0; 
-
-      // for each Card, pick another random Card (0-51) and swap them
-      for (int first = 0; first < deck.length; first++) 
-      {
-         // select a random number between 0 and 51 
-         int second =  randomNumbers.nextInt(NUMBER_OF_CARDS);
-         
-         // swap current Card with randomly selected Card
-         Card temp = deck[first];        
-         deck[first] = deck[second];   
-         deck[second] = temp;            
-      } 
-   } 
-
-   // deal one Card
-   public Card dealCard()
-   {
-      // determine whether Cards remain to be dealt
-      if (currentCard < 1)
-         return deck[currentCard++]; // return current Card in array
-      else        
-         return null; // return null to indicate that all Cards were dealt
-   } 
-} // end class DeckOfCards
+    public Card drawCard() {
+        if (deck.isEmpty()) {
+            return null;
+        }
+        int randomIndex = (int) (Math.random() * deck.size());
+        return deck.remove(randomIndex);
+    }
+}
